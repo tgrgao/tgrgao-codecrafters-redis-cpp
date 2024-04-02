3a3f638d71dc8c568dd171a9d20c6fc7205374ca
tgrgao <tigerygao@gmail.com>
2024-04-02 16:41:33 -0400

    pass 12th stage of replication

--- src/Protocol.h
+++ src/Protocol.h
@@ -3,8 +3,6 @@
 
 #include <string>
 
-std::string format_bulk_string(std::string s) {
-    return "$" + std::to_string(s.length()) + "\r\n" + s + "\r\n";
-}
+std::string format_bulk_string(std::string s);
 
 #endif
